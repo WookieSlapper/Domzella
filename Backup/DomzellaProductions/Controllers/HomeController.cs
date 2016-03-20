bc0dd5710e859f71d9e2b37f6d2f43964dfa35ae
WookieSlapper <dominicmazella@gmail.com>
2016-03-20 15:27:26 -0400

    Completed porting to MVC5; forgot to port the .js for the hex map constructor; having a .css issue with centering text in telegram div; need to work on login funcitonality to show/hide game and fleet menus( move from public nav bar); need to add data and testing projects, then add units to begin game testing; construct dice roller utility

---
+++ Backup/DomzellaProductions/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DomzellaProductions.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to the home of a programmer-in-training! This site is a work in progress. Things come and go. You'll see.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "The Abridged Version of Me";
+
+            return View();
+        }
+
+        public ActionResult Portfolio()
+        {
+            ViewBag.Message = "My Recent Work (If you like what you see, hire me!)";
+
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            ViewBag.Message = "A Place for My Ideas";
+
+            return View();
+        }
+
+        public ActionResult Coding()
+        {
+            ViewBag.Message = "A Chronological Programming Spiritual Odyssey";
+
+            return View();
+        }
+    }
+}
